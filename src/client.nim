ae9c172e4ada13540ce4c8543df3c4aa60ba9280
Jamie Atkinson <jabbslad@gmail.com>
2018-10-18 21:56:11 +0100

    initial commit

--- src/client.nim
+++ src/client.nim
@@ -1,1 +1,12 @@
+import os, threadpool
+
 echo("Chat application started")
+if paramCount() == 0:
+  quit("please specify the server address e.g. ./client localhost")
+
+let serverAddress = paramStr(1)
+echo("Connecting to ", serverAddress)
+
+while true:
+  let message = spawn stdin.readLine()
+  echo("sending \"", ^message, "\"")
